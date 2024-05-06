8f8e65480c75f1cce5596796bf4c2290dfb1d921
murugalakshmi-fsd <muthukannanml@gmail.com>
2024-05-06 06:42:09 +0530

    modifyport

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(express.json());
 app.use(bodyParser.json());
 
 app.use("/api/portfolio",portfolioroute)
-const port=process.env.PORT || 5000;
+const port=process.env.PORT;
 
 app.listen(port, ()=>{
     console.log("Server running on port",port)
